05fa840389cab3a3ef7da137c258d9cd8dfe8e64
Ayronhayd <DJ_JAZZ2010@mail.ru>
2025-03-09 19:35:48 +0300

    1.0.0 1. Модальное окно 2. Затемнения 3. Закрытие

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict';
+const initModalWindow = () => {
+    const modalWindow = document.querySelector('.modal-window');
+    const overlay = document.querySelector('.overlay');
+
+    const openModalWindow = () => {
+        modalWindow.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    };
+
+    const closeModalWindow = () => {
+        modalWindow.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
+
+    document.querySelectorAll('.show-modal-window').forEach((btn) =>
+        btn.addEventListener('click', openModalWindow)
+    );
+
+    document.querySelector('.close-modal-window').addEventListener('click', closeModalWindow);
+
+    overlay.addEventListener('click', closeModalWindow);
+};
+
+initModalWindow();
